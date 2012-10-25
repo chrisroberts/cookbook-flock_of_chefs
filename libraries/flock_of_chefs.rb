d397857914731ab6361d61ae8c7b84f76c97ada5
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-24 19:44:49 -0700

    Add new wait functionality to resources

---
+++ libraries/flock_of_chefs.rb
@@ -0,0 +1,65 @@
+module FlockOfChefs
+  class << self
+    def search(args={})
+      DCell::Node.all.find_all do |dnode|
+
+      end
+    end
+  end
+
+  module Waiter
+    def do_wait_until(pause = 1.0, &block)
+      until(block.call)
+        sleep(pause)
+      end
+    end
+
+    def do_wait_while(pause = 1.0, &block)
+      while(block.call)
+        sleep(pause)
+      end
+    end
+
+    def wait_until(pause = 1.0, &block)
+      @wait_until = {:pause => pause, :block => block}
+    end
+
+    def wait_while(pause, &block)
+      @wait_while = {:pause => pause, :block => block}
+    end
+
+    def flocked_provider_for_action(*args)
+      if(@wait_until)
+        do_wait_until(@wait_until[:pause]) do
+          @wait_until[:block].call
+        end
+      end
+      if(@wait_while)
+        do_wait_while(@wait_while[:pause]) do
+          @wait_while[:block].call
+        end
+      end
+      original_provider_for_action(*args)
+    end
+
+    class << self
+      def included(base)
+        base.class_eval do
+          unless(instance_methods.map(&:to_sym).include?(:original_provider_for_action))
+            alias_method :original_provider_for_action, :provider_for_action
+            alias_method :provider_for_action, :flocked_provider_for_action
+          end
+        end
+      end
+    end
+  end
+end
+
+unless(Chef::Resource.ancestors.include?(FlockOfChefs::Waiter))
+  klasses = Chef::Resource.constants.map{|x| Chef::Resource.const_get(x) }.find_all do |con|
+    con.is_a?(Class) && con < Chef::Resource
+  end
+  klasses.push(Chef::Resource).each do |klass|
+    klass.send(:include, FlockOfChefs::Waiter)
+  end
+end
