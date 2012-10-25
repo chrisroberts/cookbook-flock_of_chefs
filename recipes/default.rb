f97adbf56fa78d32cc56975d8d99db35154a6c15
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-24 19:49:39 -0700

    Fix up loading orders. Hook node into dcell if not already loaded

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,13 +2,37 @@
   chef_package zk_pkg
 end
 
-chef_gem 'flock_of_chefs'
-require 'flock_of_chefs'
-
+chef_gem 'zk'
 chef_gem 'celluloid-io' do
   action :install
   version '~> 0.10.0'
 end
-chef_gem 'dcell'
 
+chef_gem 'dcell'
+chef_gem 'flock_of_chefs'
+require 'flock_of_chefs'
+
+# Loads DCell up front
 node[:chef_client][:load_gems] = {:flock_of_chefs => nil}
+
+ruby_block 'Hook chefs' do
+  block do
+    FlockOfChefs.start_flocking!(node)
+  end
+  not_if do
+    DCell.me
+  end
+end
+
+# TODO: Can we get at the runner in a more direct fashion?
+ruby_block 'Flocking Chefs' do
+  block do
+    DCell.me[:flocked_resources].new_run(
+      ObjectSpace.each_object(Chef::Runner).map.first
+    )
+    Chef::Log.info 'This Chef is now flocked'
+  end
+  wait_until do
+    DCell.me
+  end
+end
