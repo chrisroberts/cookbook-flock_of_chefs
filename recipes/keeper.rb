e5672788a6c982878b8d577d4edff1c2d9a62588
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-24 19:44:06 -0700

    Move default recipe inclusion

--- recipes/keeper.rb
+++ recipes/keeper.rb
@@ -1,5 +1,3 @@
-include_recipe 'flock_of_chefs'
-
 if(node[:flock_of_chefs][:registry][:type].to_s == 'zk')
   %w(libzmq1 libzmq-dev zookeeper zookeeper-bin zookeeperd).each do |zk_pkg|
     package zk_pkg
@@ -10,3 +8,4 @@ if(node[:flock_of_chefs][:registry][:type].to_s == 'zk')
   end
 end
 
+include_recipe 'flock_of_chefs'
