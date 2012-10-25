1f46a9be694bd73a3c4e22081e69d9b2a90f3c60
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-24 19:44:33 -0700

    Define defaults for new attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
 default[:flock_of_chefs][:port] = '22222'
-default[:flock_of_chefs][:registry][:type] = nil # gossip by default
+default[:flock_of_chefs][:registry][:type] = 'zk'
 default[:flock_of_chefs][:example] = {}
+default[:flock_of_chefs][:bind_addr][:device] = 'eth1'
+default[:flock_of_chefs][:registry] = nil
