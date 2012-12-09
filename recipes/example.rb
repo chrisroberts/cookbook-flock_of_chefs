71970099de2120ae137c51b33f49f3881bc6ffbf
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-09 07:45:42 -0800

    Add readme

--- recipes/example.rb
+++ recipes/example.rb
@@ -1,15 +1,11 @@
-
 if(node.name == 'zkslave.chrisroberts')
   file '/opt/sub-test' do
     action :nothing
     content "Received at: #{Time.now}"
-    wait_until do
-      FlockOfChefs.me
-    end
+    remote_subscribes :create, 'file[/opt/notify-test]', :node => 'zkslave2.chrisroberts'
   end
 else
-  file '/opt/not-test' do
+  file '/opt/notify-test' do
     content 'test'
-    remote_notifies :create, 'file[/opt/sub-test]', :node => 'zkslave.chrisroberts'
   end
 end
