69436133a6d32ace7b7bba98ff3b36019ae0e48a
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-09 07:48:33 -0800

    Allow basic configuration to basic example

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,5 @@ default[:flock_of_chefs][:enabled] = true
 default[:flock_of_chefs][:port] = '22222'
 default[:flock_of_chefs][:bind_addr][:device] = nil
 default[:flock_of_chefs][:packages] = %w(libzmq-dev)
+default[:flock_of_chefs][:example][:subscribe_node] = 'zkslave.chrisroberts'
+default[:flock_of_chefs][:example][:notify_node] = 'zkslave2.chrisroberts'
--- recipes/example.rb
+++ recipes/example.rb
@@ -1,8 +1,8 @@
-if(node.name == 'zkslave.chrisroberts')
+if(node.name == node[:flock_of_chefs][:example][:subscribe_node])
   file '/opt/sub-test' do
     action :nothing
     content "Received at: #{Time.now}"
-    remote_subscribes :create, 'file[/opt/notify-test]', :node => 'zkslave2.chrisroberts'
+    remote_subscribes :create, 'file[/opt/notify-test]', :node => node[:flock_of_chefs][:example][:notify_node]
   end
 else
   file '/opt/notify-test' do
